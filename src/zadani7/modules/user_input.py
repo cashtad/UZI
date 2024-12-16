401771e69d1bb0d0a15f1f3a0dc5cd7f66c83bbb
cashtad <lemalaxz2@gmail.com>
2024-12-16 15:05:40 +0100

    курсач

--- src/zadani7/modules/user_input.py
+++ src/zadani7/modules/user_input.py
@@ -1,13 +1,32 @@
 def get_user_input():
-    deposit = float(input("Zadejte částku vkladu (CZK): "))
-    transactions = int(input("Zadejte počet transakcí kartou za měsíc: "))
-    investment = float(input("Jste ochotni měsíčně investovat? Pokud ano, zadejte částku (CZK), jinak 0: "))
+    deposit = 0
+    while True:
+        try:
+            deposit = float(input("Zadejte částku vkladu (CZK): "))
+            break
+        except ValueError:
+            print("Nesprávný vstup. Zadejte číslo!")
+    transactions = 0
+    while True:
+        try:
+            transactions = int(input("Zadejte počet transakcí kartou za měsíc: "))
+            break
+        except ValueError:
+            print("Nesprávný vstup. Zadejte číslo!")
+
+    investment = 0
+    while True:
+        try:
+            investment = float(input("Jste ochotni měsíčně investovat? Pokud ano, zadejte částku (CZK), jinak 0: "))
+            break
+        except ValueError:
+            print("Nesprávný vstup. Zadejte číslo!")
 
     city_info = None
-    has_branch = input("Je pro vás důležité mít pobočku banky ve vašem městě? (ano/ne): ").strip().lower()
+    has_branch = input("Je pro vás důležité mít pobočku banky ve vašem nebo nejbližším k vám městě? (ano/ne): ").strip().lower()
     if has_branch == "ano":
         city = input("Zadejte název vašeho města: ")
         nearest_large_city = input("Jaké je nejbližší velké město? ")
-        city_info = (city, nearest_large_city)
+        city_info = [city, nearest_large_city]
 
     return deposit, transactions, investment, city_info
