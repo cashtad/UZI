f7c77aa24a114c21b645156ef932395a54b7143b
cashtad <lemalaxz2@gmail.com>
2024-12-16 15:13:56 +0100

    курсач

--- src/zadani7/modules/decision_maker.py
+++ src/zadani7/modules/decision_maker.py
@@ -4,22 +4,23 @@ from src.zadani7.modules.bonus_checker import check_bonus_conditions
 def choose_best_bank(banks, deposit, transactions, investment):
     best_bank = None
     best_rate = 0
-    explanation = ""
     reason = ""
 
     for bank in banks:
         basic_rate = float(bank["interest_rate_basic"].replace(",", "."))
         bonus_rate = float(bank["interest_rate_bonus"].replace(",", "."))
-
+        reason_test = ""
         if check_bonus_conditions(bank, deposit, transactions, investment):
             rate = bonus_rate
-            reason = "Bonusová sazba je dostupná díky splnění všech podmínek."
+            reason_test = "Bonusová sazba je dostupná díky splnění všech podmínek."
         else:
             rate = basic_rate
-            reason = "Bonusová sazba není dostupná; uvažuje se základní sazba."
+            reason_test = "Bonusová sazba není dostupná, uvažuje se základní sazba."
 
         if rate > best_rate:
             best_bank = bank
             best_rate = rate
+            reason = reason_test
+
 
     return best_bank, best_rate, reason
